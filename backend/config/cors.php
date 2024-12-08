f3e79d61db5902edf1ec958cce43bf70aa2606ae
amdebirhan asmamaw <amdeasme7@gmail.com>
2024-12-08 16:45:41 +0300

    deleted unnessary asset files

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://your-frontend-domain.onrender.com'],
 
     'allowed_origins_patterns' => ['*'],
 
